ebc7414a0db159d5a9bf91941903f88aaf647438
KHANH PHUONG <kphuonght22@gmail.com>
2021-07-25 16:51:07 +0700

    completed

---
+++ precache-manifest.52f03097dd748594d5cc7770fee3d196.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8be85d69dbaf46cd7908457fcbe6bf2d",
+    "url": "/Va2/react-todo-list/index.html"
+  },
+  {
+    "revision": "c71a387a016f068d3f70",
+    "url": "/Va2/react-todo-list/static/css/2.d7892326.chunk.css"
+  },
+  {
+    "revision": "050227d3915a844b4703",
+    "url": "/Va2/react-todo-list/static/css/main.498b29ef.chunk.css"
+  },
+  {
+    "revision": "c71a387a016f068d3f70",
+    "url": "/Va2/react-todo-list/static/js/2.25682155.chunk.js"
+  },
+  {
+    "revision": "050227d3915a844b4703",
+    "url": "/Va2/react-todo-list/static/js/main.2bf987ce.chunk.js"
+  },
+  {
+    "revision": "caf3604553b0f10db1d9",
+    "url": "/Va2/react-todo-list/static/js/runtime~main.6bddfa51.js"
+  },
+  {
+    "revision": "ed6b6100b16162a2785dd2474d362f19",
+    "url": "/Va2/react-todo-list/static/media/Team1.ed6b6100.jpg"
+  }
+]);
